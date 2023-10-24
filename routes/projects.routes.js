a1c1dd4ea7ee86dd428058274983a5e0f57c1b13
Khalil <khalilaahmd@gmail.com>
2023-10-24 22:21:53 +0200

    Update 24-10-23

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -13,8 +13,8 @@ router.get('/christmasDesigns', (req, res, next) => {
         .then((christmasDesignFromDB) => res.status(200).json(christmasDesignFromDB))
         .catch((error) => next(error));
 })
-// GET "/api/christmasDesign/:christmasDesignId"
-router.get('/api/christmasDesign/:christmasDesignId', (req, res, next) => {
+// GET "/api/christmasDesigns/:christmasDesignId"
+router.get('/christmasDesigns/:christmasDesignId', (req, res, next) => {
     const { christmasDesignId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(christmasDesignId)) {
         res.status(400).json({ message: "Specified Id is not valid"});
@@ -26,6 +26,25 @@ router.get('/api/christmasDesign/:christmasDesignId', (req, res, next) => {
         .catch(error => res.json(error));
 })
 
+// DELETE "/api/christmasDesigns/:christmasDesignId"
+router.delete('/christmasDesigns/:christmasDesignId', (req, res, next) => {
+    const { christmasDesignId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(christmasDesignId)) {
+        res.status(400).json({ message: 'specified id is not valid'});
+        return;
+    }
+    Project
+         .findByIdAndRemove(christmasDesignId)
+         .then((deleteImage) => {
+            if(!deleteImage) {
+                res.status(404).json({ message: 'Image not found'});
+                return;
+            }
+            res.json({message: `ChristmasDesign with ${christmasDesignId} is removed successfully`});
+        })
+        .catch(error => res.status(500).json({ message: 'Error deleting image', error: error }));
+});
+
 // GET "/api/artProjects"
 router.get('/artProjects', (req, res, next) => {
     Project
@@ -34,7 +53,7 @@ router.get('/artProjects', (req, res, next) => {
         .catch((error) => next(error));
 })
 // GET "/api/artProjects/:artProjectsId"
-router.get('/api/artProjects/:artProjectsId', (req, res, next) => {
+router.get('/artProjects/:artProjectsId', (req, res, next) => {
     const { artProjectsId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(artProjectsId)) {
         res.status(400).json({ message: "Specified Id is not valid"});
@@ -46,6 +65,26 @@ router.get('/api/artProjects/:artProjectsId', (req, res, next) => {
         .catch(error => res.json(error));
 })
 
+// DELETE "/api/artProjects/:artProjectsId"
+router.delete('/artProjects/:artProjectsId', (req, res, next) => {
+    const { artProjectsId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(artProjectsId)) {
+        res.status(400).json({ message: 'specified id is not valid'});
+        return;
+    }
+    Project
+         .findByIdAndRemove(artProjectsId)
+         .then((deleteImage) => {
+            if(!deleteImage) {
+                res.status(404).json({ message: 'Image not found'});
+                return;
+            }
+            res.json({message: `Art Project with ${artProjectsId} is removed successfully`});
+        })
+        .catch(error => res.status(500).json({ message: 'Error deleting image', error: error }));
+});
+
+
 // GET "/api/specialEvents"
 router.get('/specialEvents', (req, res, next) => {
     Project
@@ -54,7 +93,7 @@ router.get('/specialEvents', (req, res, next) => {
         .catch((error) => next(error));
 })
 // GET "/api/specialEvents/:specialEventsId"
-router.get('/api/specialEvents/:specialEventsId', (req, res, next) => {
+router.get('/specialEvents/:specialEventsId', (req, res, next) => {
     const { specialEventsId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(specialEventsId)) {
         res.status(400).json({ message: "Specified Id is not valid"});
@@ -66,6 +105,25 @@ router.get('/api/specialEvents/:specialEventsId', (req, res, next) => {
         .catch(error => res.json(error));
 })
 
+// DELETE "/api/specialEvents/:specialEventsId"
+router.delete('/specialEvents/:specialEventsId', (req, res, next) => {
+    const { specialEventsId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(specialEventsId)) {
+        res.status(400).json({ message: 'specified id is not valid'});
+        return;
+    }
+    Project
+         .findByIdAndRemove(specialEventsId)
+         .then((deleteImage) => {
+            if(!deleteImage) {
+                res.status(404).json({ message: 'Image not found'});
+                return;
+            }
+            res.json({message: `Special Event with ${specialEventsId} is removed successfully`});
+        })
+        .catch(error => res.status(500).json({ message: 'Error deleting image', error: error }));
+});
+
 // GET "/api/boutonniersAndGifts"
 router.get('/boutonniersAndGifts', (req, res, next) => {
     Project
@@ -73,8 +131,8 @@ router.get('/boutonniersAndGifts', (req, res, next) => {
         .then((boutonniersAndGiftsFromDB) => res.status(200).json(boutonniersAndGiftsFromDB))
         .catch((error) => next(error));
 })
-// GET "/api/specialEvents/:specialEventsId"
-router.get('/api/boutonniersAndGifts/:boutonniersAndGiftsId', (req, res, next) => {
+// GET "/api/boutonniersAndGifts/:boutonniersAndGiftsId"
+router.get('/boutonniersAndGifts/:boutonniersAndGiftsId', (req, res, next) => {
     const { boutonniersAndGiftsId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(boutonniersAndGiftsId)) {
         res.status(400).json({ message: "Specified Id is not valid"});
@@ -86,6 +144,25 @@ router.get('/api/boutonniersAndGifts/:boutonniersAndGiftsId', (req, res, next) =
         .catch(error => res.json(error));
 })
 
+// DELETE "/api/boutonniersAndGifts/:boutonniersAndGiftsId"
+router.delete('boutonniersAndGifts/:boutonniersAndGiftsId', (req, res, next) => {
+    const { boutonniersAndGiftsId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(boutonniersAndGiftsId)) {
+        res.status(400).json({ message: 'specified id is not valid'});
+        return;
+    }
+    Project
+         .findByIdAndRemove(boutonniersAndGiftsId)
+         .then((deleteImage) => {
+            if(!deleteImage) {
+                res.status(404).json({ message: 'Image not found'});
+                return;
+            }
+            res.json({message: `Boutonniers And Gifts ID with ${boutonniersAndGiftsId} is removed successfully`});
+        })
+        .catch(error => res.status(500).json({ message: 'Error deleting image', error: error }));
+});
+
 // GET "/api/flowers"
 router.get('/flowers', (req, res, next) => {
     Project
@@ -94,7 +171,7 @@ router.get('/flowers', (req, res, next) => {
         .catch((error) => next(error));
 })
 // GET "/api/flowers/:flowersId"
-router.get('/api/flowers/:flowersId', (req, res, next) => {
+router.get('/flowers/:flowersId', (req, res, next) => {
     const { flowersId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(flowersId)) {
         res.status(400).json({ message: "Specified Id is not valid"});
@@ -106,6 +183,25 @@ router.get('/api/flowers/:flowersId', (req, res, next) => {
         .catch(error => res.json(error));
 })
 
+// DELETE "/api/flowers/:flowersId"
+router.delete('flowers/:flowersId', (req, res, next) => {
+    const { flowersId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(flowersId)) {
+        res.status(400).json({ message: 'specified id is not valid'});
+        return;
+    }
+    Project
+         .findByIdAndRemove(flowersId)
+         .then((deleteImage) => {
+            if(!deleteImage) {
+                res.status(404).json({ message: 'Image not found'});
+                return;
+            }
+            res.json({message: `Flowers ID with ${flowersId} is removed successfully`});
+        })
+        .catch(error => res.status(500).json({ message: 'Error deleting image', error: error }));
+});
+
 // GET "/api/bouquets"
 router.get('/bouquets', (req, res, next) => {
     Project
@@ -114,7 +210,7 @@ router.get('/bouquets', (req, res, next) => {
         .catch((error) => next(error));
 })
 // GET "/api/bouquets/:bouquetsId"
-router.get('/api/bouquets/:bouquetsId', (req, res, next) => {
+router.get('/bouquets/:bouquetsId', (req, res, next) => {
     const { bouquetsId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(bouquetsId)) {
         res.status(400).json({ message: "Specified Id is not valid"});
@@ -125,6 +221,25 @@ router.get('/api/bouquets/:bouquetsId', (req, res, next) => {
         .then(project => res.status(200).json(project))
         .catch(error => res.json(error));
 })
+
+// DELETE "/api/bouquets/:bouquetsId"
+router.delete('bouquets/:bouquetsId', (req, res, next) => {
+    const { bouquetsId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(bouquetsId)) {
+        res.status(400).json({ message: 'specified id is not valid'});
+        return;
+    }
+    Project
+         .findByIdAndRemove(bouquetsId)
+         .then((deleteImage) => {
+            if(!deleteImage) {
+                res.status(404).json({ message: 'Image not found'});
+                return;
+            }
+            res.json({message: `Bouquets ID with ${bouquetsId} is removed successfully`});
+        })
+        .catch(error => res.status(500).json({ message: 'Error deleting image', error: error }));
+});
 
 // POST "/api/uploadProject"
 router.post("/uploadProject", fileUploader.single("postUrl"), (req, res, next) => {
